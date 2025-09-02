0e7e737cf79389a1f5da7d39a2a15d0d052153aa
Yordanos Shanbel <yordanosshanbel28@gmail.com>
2025-09-01 21:21:49 -0300

    create seperate file for products

---
+++ product.js
@@ -0,0 +1,84 @@
+let product = [
+  {
+    id: 0,
+    shortName: 'Waffle',
+    fullName: 'Waffle with Beries',
+    price: '6.50',
+    imgSourceMobile: 'images/image-waffle-mobile.jpg',
+    imgSourceTable: 'images/image-waffle-tablet.jpg',
+    imgSourceDesktop: 'images/image-waffle-desktop.jpg'
+  },
+  {
+    id: 1,
+    shortName: 'Creme Brulee',
+    fullName: 'Vanilla Bean Creme Brulee',
+    price: '7.00',
+    imgSourceMobile: 'images/image-creme-brulee-mobile.jpg',
+    imgSourceTable: 'images/image-creme-brulee-tablet.jpg',
+    imgSourceDesktop: 'images/image-creme-brulee-desktop.jpg'
+  },
+  {
+    id: 2,
+    shortName: 'Macaron',
+    fullName: 'Macaron Mix of Five',
+    price: '8.00',
+    imgSourceMobile: 'images/image-macaron-mobile.jpg',
+    imgSourceTable: 'images/image-macaron-tablet.jpg',
+    imgSourceDesktop: 'images/image-macaron-desktop.jpg'
+  },
+  {
+    id: 3,
+    shortName: 'Tiramisu',
+    fullName: 'Classic Tiramisu',
+    price: '5.50',
+    imgSourceMobile: 'images/image-tiramisu-mobile.jpg',
+    imgSourceTable: 'images/image-tiramisu-tablet.jpg',
+    imgSourceDesktop: 'images/image-tiramisu-desktop.jpg'
+  },
+  {
+    id: 4,
+    shortName: 'Baklava',
+    fullName: 'Pistachio Baklava',
+    price: '4.00',
+    imgSourceMobile: 'images/image-baklava-mobile.jpg',
+    imgSourceTable: 'images/image-baklava-tablet.jpg',
+    imgSourceDesktop: 'images/image-baklava-desktop.jpg'
+  },
+  {
+    id: 5,
+    shortName: 'Pie',
+    fullName: 'Lemon Meringue Pie',
+    price: '5.00',
+    imgSourceMobile: 'images/image-meringue-mobile.jpg',
+    imgSourceTable: 'images/image-meringue-tablet.jpg',
+    imgSourceDesktop: 'images/image-meringue-desktop.jpg'
+  },
+  {
+    id: 6,
+    shortName: 'Cake',
+    fullName: 'Red Velvet Cake',
+    price: '4.50',
+    imgSourceMobile: 'images/image-cake-mobile.jpg',
+    imgSourceTable: 'images/image-cake-tablet.jpg',
+    imgSourceDesktop: 'images/image-cake-desktop.jpg'
+  },
+
+  {
+    id: 7,
+    shortName: 'Brownie',
+    fullName: 'Salted Caramel Brownie',
+    price: '5.50',
+    imgSourceMobile: 'images/image-brownie-mobile.jpg',
+    imgSourceTable: 'images/image-brownie-tablet.jpg',
+    imgSourceDesktop: 'images/image-brownie-desktop.jpg'
+  },
+  {
+    id: 8,
+    shortName: 'Panna Cotta',
+    fullName: 'Vanilla Panna Cotta',
+    price: '6.50',
+    imgSourceMobile: 'images/image-panna-cotta-mobile.jpg',
+    imgSourceTable: 'images/image-panna-cotta-tablet.jpg',
+    imgSourceDesktop: 'images/image-panna-cotta-desktop.jpg'
+  }
+];
